86267532cf4ed3c5ca16f4d3eeb9960b5c5e53f6
chenyan1999 <chenyanliu712@gmail.com>
2025-03-13 12:47:52 +0800

    modify locator

--- backend/trace/locator.py
+++ backend/trace/locator.py
@@ -233,13 +233,6 @@ def predict_sliding_windows(prev_edit_hunks, locator, locator_tokenizer, commit_
     Return:
         None. The result is stored in raw_preds, the time cost is saved in record    
     """
-    filtered_sliding_windows = []
-    for window in sliding_windows:
-        if ("keras/layers/core.py" in window["file_path"] or "keras\\layers\\core.py" in window["file_path"]) and window["start_line_idx"] > 110:
-            continue
-        filtered_sliding_windows.append(window)
-    sliding_windows = filtered_sliding_windows
-
     locator_dateset_one_file = make_locator_dataset(sliding_windows, prev_edit_hunks,locator_tokenizer,commit_msg)
     locator_dataloader = DataLoader(locator_dateset_one_file, batch_size=20, shuffle=False)
             
@@ -275,6 +268,9 @@ def predict_sliding_windows(prev_edit_hunks, locator, locator_tokenizer, commit_
         if sliding_window["file_path"] not in locator_response:
             locator_response[sliding_window["file_path"]] = []
 
+        if ("keras/layers/core.py" in sliding_window["file_path"] or "keras\\layers\\core.py" in sliding_window["file_path"]) and sliding_window["start_line_idx"] > 110:
+                continue
+            
         locator_response[sliding_window["file_path"]].append({
             "code_window_start_line": sliding_window["start_line_idx"],
             "inline_labels": inline_preds,
